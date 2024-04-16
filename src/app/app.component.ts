3849683b93c69ec1654b82bf0f262f5cf336927b
Haziel Ortiz <ya-yo22124@outlook.com>
2024-04-15 21:55:35 -0600

    Add: Components and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
+import { ChartCardComponent } from './components/chart-card/chart-card.component';
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { MiPrimerComponenteComponent } from './components/mi-primer-componente/mi-primer-componente.component';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MiPrimerComponenteComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ChartCardComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'GuiaBasicaAngular';
+  public miNombre: string = 'Eli Haziel Ortiz Ramírez';
 }
---
+++ src/app/components/mi-primer-componente/mi-primer-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mi-primer-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './mi-primer-componente.component.html',
+  styleUrl: './mi-primer-componente.component.scss'
+})
+export class MiPrimerComponenteComponent {
+
+}
